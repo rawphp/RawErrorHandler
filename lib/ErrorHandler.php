f02f0d2c1fc1da315a6d5b673f4828b7c1a4283d
Tom Kaczocha <tom@rawphp.org>
2014-09-12 23:25:15 +1000

    Initial code commit.

---
+++ lib/ErrorHandler.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * This file is part of RawPHP - a PHP Framework.
+ * 
+ * Copyright (c) 2014 RawPHP.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * PHP version 5.4
+ * 
+ * @category  PHP
+ * @package   RawPHP/RawErrorHandler
+ * @author    Tom Kaczohca <tom@rawphp.org>
+ * @copyright 2014 Tom Kaczocha
+ * @license   http://rawphp.org/license.txt MIT
+ * @link      http://rawphp.org/
+ */
+
+namespace RawPHP\RawErrorHandler;
+
+use RawPHP\RawErrorHandler\IErrorHandler;
+use RawPHP\RawBase\Component;
+
+/**
+ * An error handler service.
+ * 
+ * @category  PHP
+ * @package   RawPHP/RawErrorHandler
+ * @author    Tom Kaczocha <tom@rawphp.org>
+ * @copyright 2014 Tom Kaczocha
+ * @license   http://rawphp.org/license.txt MIT
+ * @link      http://rawphp.org/
+ */
+class ErrorHandler extends Component implements IErrorHandler
+{
+    public $errorCallback     = NULL;
+    public $exceptionCallback = NULL;
+    public $shutdownCallback  = NULL;
+    
+    /**
+     * Error Handler Constructor.
+     * 
+     * @param array $config configuration array
+     */
+    public function __construct( $config = array( ) )
+    {
+        parent::__construct();
+        
+        $this->init( $config );
+    }
+    
+    /**
+     * Initialises the error handler.
+     * 
+     * @param array $config configuration array
+     * 
+     * @uses set_error_handler     to set the error handler
+     * @uses set_exception_handler to set the exception handler
+     */
+    public function init( $config = array( ) )
+    {
+        if ( isset( $config[ 'error_callback' ] ) )
+        {
+            $this->errorCallback = $config[ 'error_callback' ];
+        }
+        
+        if ( isset( $config[ 'exception_callback' ] ) )
+        {
+            $this->exceptionCallback = $config[ 'exception_callback' ];
+        }
+        
+        if ( isset( $config[ 'shutdown_callback' ] ) )
+        {
+            $this->shutdownCallback = $config[ 'shutdown_callback' ];
+            
+            register_shutdown_function( $this->shutdownCallback );
+        }
+        
+        set_error_handler( array( $this, 'handleError' ), E_ALL );
+        
+        set_exception_handler( array( $this, 'handleException' ) );
+    }
+
+    /**
+     * Handles PHP errors that may occur.
+     * 
+     * @param int    $errno   the error no
+     * @param string $message error message
+     * @param string $file    the file name
+     * @param int    $line    the line number
+     * @param array  $context the error context
+     * 
+     * @return bool TRUE
+     */
+    public function handleError( $errno, $message = '', $file = '', $line = '', $context = '' )
+    {
+        // restore previous error handler
+        restore_error_handler();
+        
+        if ( $errno === NULL )
+        {
+            $error = error_get_last();
+            
+            $error = array(
+                'file'      => $error[ 'file' ],
+                'line'      => $error[ 'line' ],
+                'message'   => $error[ 'message' ],
+                'trace'     => $this->getBacktrace( debug_backtrace() ),
+            );
+        }
+        else
+        {
+            $error = array(
+                'message' => $message,
+                'file' => $file,
+                'line'  => $line,
+                'context' => $context,
+            );
+        }
+        
+        if ( NULL !== $this->errorCallback )
+        {
+            call_user_func_array( $this->errorCallback, $error );
+        }
+        
+        return TRUE;
+    }
+
+    /**
+     * Handles exceptions.
+     * 
+     * @param \Exception $exception the exception
+     * 
+     * @return bool TRUE
+     */
+    public function handleException( \Exception $exception )
+    {
+        // restore previous exception handler
+        restore_exception_handler();
+        
+        if ( $exception instanceof \Exception )
+        {
+            $error = array(
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'message' => $exception->getMessage(),
+                'trace' => $this->getBacktrace( $exception->getTrace() ),
+            );
+        }
+        else
+        {
+            return;
+        }
+        
+        if ( NULL !== $this->exceptionCallback )
+        {
+            call_user_func_array( $this->exceptionCallback, $error );
+        }
+        
+        return TRUE;
+    }
+
+    /**
+     * Returns a list of traces for the error or exception.
+     * 
+     * @param array $trace original debug backtrace
+     * 
+     * @return array processed backtrace
+     */
+    public function getBacktrace( $trace )
+    {
+        $traces = array();
+        
+        $i = 1;
+
+        foreach ( $trace as $t )
+        {
+            $traceString = '';
+
+            if ( !isset( $t[ 'file' ] ) )
+            {
+                $t[ 'file' ] = 'unknown';
+            }
+            if ( !isset( $t[ 'line' ] ) )
+            {
+                $t[ 'line' ] = 0;
+            }
+            if ( !isset( $t[ 'function' ] ) )
+            {
+                $t[ 'function' ] = 'unknown';
+            }
+
+            $traceString .= '#' .
+                    $i . ' ' .
+                    $t[ 'file' ] . '(' .
+                    $t[ 'line' ] . '): ';
+
+            if ( isset( $t[ 'object' ] ) && is_object( $t[ 'object' ] ) )
+            {
+                $traceString .= get_class( $t[ 'object' ] ) . '->';
+            }
+
+            $traceString .= $t[ 'function' ] . '()';
+
+            $traces[] = $traceString;
+
+            $i++;
+        }
+
+        return $traces;
+    }
+
+}
---
+++ lib/interfaces/IErrorHandler.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of RawPHP - a PHP Framework.
+ * 
+ * Copyright (c) 2014 RawPHP.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * PHP version 5.4
+ * 
+ * @category  PHP
+ * @package   RawPHP/RawErrorHandler
+ * @author    Tom Kaczohca <tom@rawphp.org>
+ * @copyright 2014 Tom Kaczocha
+ * @license   http://rawphp.org/license.txt MIT
+ * @link      http://rawphp.org/
+ */
+
+namespace RawPHP\RawErrorHandler;
+
+/**
+ * An error handler interface.
+ * 
+ * @category  PHP
+ * @package   RawPHP/RawErrorHandler
+ * @author    Tom Kaczocha <tom@rawphp.org>
+ * @copyright 2014 Tom Kaczocha
+ * @license   http://rawphp.org/license.txt MIT
+ * @link      http://rawphp.org/
+ */
+interface IErrorHandler
+{
+    /**
+     * Initialises the error handler.
+     * 
+     * @param array $config configuration array
+     * 
+     * @uses set_error_handler     to set the error handler
+     * @uses set_exception_handler to set the exception handler
+     */
+    public function init( $config = array( ) );
+    
+    /**
+     * Handles PHP errors that may occur.
+     * 
+     * @param int    $errno   the error no
+     * @param string $message error message
+     * @param string $file    the file name
+     * @param int    $line    the line number
+     * @param array  $context the error context
+     * 
+     * @return bool TRUE on success, FALSE on failure
+     */
+    public function handleError( $errno, $message = '', $file = '', $line = '', $context = '' );
+    
+    /**
+     * Handles exceptions.
+     * 
+     * @param \Exception $exception the exception
+     * 
+     * @return bool TRUE on success, FALSE on failure
+     */
+    public function handleException( \Exception $exception );
+    
+    /**
+     * Returns a list of traces for the error or exception.
+     * 
+     * @param array $trace original debug backtrace
+     * 
+     * @return array processed backtrace
+     */
+    public function getBacktrace( $trace );
+}
